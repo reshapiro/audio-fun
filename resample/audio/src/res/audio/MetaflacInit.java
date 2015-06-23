fc047acbf6e05caff304846e5c4daa45b47f5c7a
R Shapiro <rshapiro@bbn.com>
2015-06-23 12:42:15 -0400

    Rework this to be file based

--- resample/audio/src/res/audio/MetaflacInit.java
+++ resample/audio/src/res/audio/MetaflacInit.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class MetaflacInit {
 
 	/**
-	 * Initialize flac metadata for given directories
+	 * Create flac metadata files for each flac file in a given directory.
 	 * @param args one arg, the name of the directory containing flac files.
 	 */
 	
@@ -20,75 +20,102 @@ public class MetaflacInit {
 		TRACKNUMBER,
 		TRACKTOTAL,
 		TITLE;
-		void put(List<String> tags, String value) {
-			tags.add(name() + "=" + value);
+		void print(String value, PrintWriter printer) {
+			printer.println(name() + "=" + value);
 		}
 	}
-	private final File directory;
-	private final List<String> tags = new ArrayList<>();
+	private final File flacDir;
+	private final File metaFlacDir;
+	private final int total;
+	private final File[] flacFiles;
 	private int index;
+	private final List<String> otherValues = new ArrayList<>();
 	
-	MetaflacInit(String path) {
-		directory = new File(path);
-	}
-	
-	private List<String> processDirectory() {
-		if (!directory.isDirectory()) {
-			fail(directory + " is not a directory");
-			return tags;
-		}
-		TAG_NAMES.ALBUM.put(tags, directory.getName());
+	MetaflacInit(String...args) {
 		
-		String[] files = directory.list(new FilenameFilter() {
+		String flacDirPath = args[0];
+		String metaFlacDirPath = args[1];
+		for (int i=2; i< args.length; i++) {
+			otherValues.add(args[i]);
+		}
+		
+		metaFlacDir = new File(metaFlacDirPath);
+		
+		if (!metaFlacDir.isDirectory() && !metaFlacDir.mkdirs()) {
+			fail(metaFlacDirPath + "does not exist and cannot be created");
+		}
+		
+		this.flacDir = new File(flacDirPath);
+		if (!flacDir.isDirectory()) {
+			fail(flacDirPath + " is not a directory");
+		}
+		flacFiles = flacDir.listFiles(new FilenameFilter() {
 			@Override
 			public boolean accept(File dir, String name) {
 				return name.endsWith(".flac");
 			}
 		});
+		total = flacFiles != null ? flacFiles.length : 0;
 		
-		if (files != null) {
-			for (String filename : files) {
-				int dot = filename.indexOf('.');
-				String name = filename.substring(0, dot);
-				addTrack(name);
-			}
-		}
+	}
 	
-		TAG_NAMES.TRACKTOTAL.put(tags, Integer.toString(index));
-		return tags;
-	}
-
-	private void addTrack(String name) {
-		TAG_NAMES.TRACKNUMBER.put(tags, Integer.toString(++index));
-		TAG_NAMES.TITLE.put(tags, name);
-	}
-
-	public static void main(String[] args) {
-		if (args.length < 2) {
-			fail("A directory and output file must be provided");
-		}
-		
-		String directory = args[0];
-		String outputFile = args[1];
-		MetaflacInit metaflacInit = new MetaflacInit(directory);
-		List<String> tags = metaflacInit.processDirectory();
-		
-		try (PrintWriter writer = new PrintWriter(new FileWriter(outputFile))) {
-			for (String tag : tags) {
-				writer.println(tag);
+	private void processDirectory() {
+		if (flacFiles != null) {
+			for (File flacFile : flacFiles) {
+				createMetaData(flacFile);
 			}
-			System.out.println("Wrote metaflac file to " + outputFile);
+		}
+	}
+
+	private void createMetaData(File flacFile) {
+		String name = flacFile.getName();
+		String title = name = name.substring(0, name.indexOf('.'));
+		String metaFileName =  title + ".mfl";
+		File metFlacFile = new File(metaFlacDir, metaFileName);
+		
+		try (PrintWriter writer = new PrintWriter(new FileWriter(metFlacFile))) {
+			TAG_NAMES.ALBUM.print(flacDir.getName(), writer);
+			TAG_NAMES.TRACKTOTAL.print(Integer.toString(total), writer);
+			TAG_NAMES.TRACKNUMBER.print( Integer.toString(++index), writer);
+			TAG_NAMES.TITLE.print(title, writer);
+			addOtherValues(writer);
+			
 		} catch (IOException e) {
-			fail("Failed to write results to " + outputFile +  ":" + e.getMessage());
+			fail(e.getMessage());
 		}
 		
-		
-
 	}
-	
+
+	private void addOtherValues(PrintWriter writer) {
+		for (String value : otherValues) {
+			writer.println(value);
+		}
+		
+	}
+
 	private static void fail(String message) {
 		System.err.println(message);
 		System.exit(-1);
+	}
+
+	/**
+	 * 
+	 * @param args two required args + any number of optional args
+	 * arg1 should be the path to a directory of .flac files.
+	 * arg2 should be a path to a directory for the genertated metadata.  This directory will be created if it doesn't already exist.
+	 * 
+	 * The remaining args should be name=value strings where the name is a  valid FLAC tag.
+	 * If an optional value includes any spaces you must quote it: ARTIST="Ornette Coleman", not  ARTIST=Ornette Coleman
+	 */
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			fail("A flac directory and metaflac file directory must be provided");
+		}
+		
+		
+		MetaflacInit metaflacInit = new MetaflacInit(args);
+		
+		metaflacInit.processDirectory();
 	}
 
 }
