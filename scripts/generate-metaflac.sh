076af2b593a2205939734041e418d5b79f1c2187
R Shapiro <rshapiro@bbn.com>
2015-06-24 20:42:47 -0400

    Improved version of metaflac generation

--- scripts/generate-metaflac.sh
+++ scripts/generate-metaflac.sh
@@ -4,12 +4,36 @@ metaflacdir=$2
 
 mkdir "$metaflacdir"
 
+total=0
 for f in $flacdir/*.flac
  do
-  echo " file is $f"
+   total=`expr $total + 1`
+ done
+
+
+index=1
+for f in $flacdir/*.flac
+ do
+ # echo " file is $f"
   base=`basename "$f" ".flac"`
-  echo " basename is  $base"
+  # echo " basename is  $base"
   metaflacfile="$metaflacdir"/"$base".mfl
-  echo "mf file is "$metaflacfile""
-  echo "TITLE=$base" >> "$metaflacfile"
+#  echo "mf file is "$metaflacfile""
+  echo "TITLE"=$base"" >> "$metaflacfile"
+  echo "TRACKNUMBER=$index" >> "$metaflacfile"
+  echo "TRACKTOTAL=$total" >> "$metaflacfile"
+ # add the varargs should probably be a function. 
+ 
+ 
+  vardex=1
+  for var in "$@"
+do
+  if [ $vardex -ge 3 ]; then 
+     echo "$var" >> "$metaflacfile"
+  fi
+  vardex=`expr $vardex + 1`
+done
+  
+  
+  index=`expr $index + 1`
 done
