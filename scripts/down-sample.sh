e131f868f22657223bfde430c5b5858fa20bfc89
R Shapiro <rshapiro@bbn.com>
2015-06-26 08:19:32 -0400

    Fix syntax errors

--- scripts/down-sample.sh
+++ scripts/down-sample.sh
@@ -6,13 +6,13 @@ if [ $# -lt 2 ]; then
     
     echo "The first argument should be a directory containing hi-def audio files."
     echo "The second argument should name the directory which will be created and will contain a down-sampled file for each hi-def file"
-    echo "An optional third argument can be used to specify the sample rate if you don't want  to sample at 48k rather 44100
-    echo "An optional fourth argument can be used to specify the sample size if you don't want 16
+    echo "An optional third argument can be used to specify the sample rate if you don't want  to sample at 48k rather 44100"
+    echo "An optional fourth argument can be used to specify the sample size if you don't want 16"
     
     exit -1
 fi
-dir=$1
-newdir=$2
+dir="$1"
+newdir="$2"
 #default rate and size
 
 rate=44100
@@ -26,8 +26,8 @@ if [ $# -gt 3 ]; then
    size=$4
 fi
 
-f [  ! -d "$dir" ]; then
-  echo "The directory $dir does not exist"
+if [  ! -d "$dir" ]; then
+  echo "The directory "$dir" does not exist"
   exit -2
 fi
 
