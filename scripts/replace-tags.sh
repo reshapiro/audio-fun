1242b40dac686d2931ff98a8944ef63ab0aa38c3
R Shapiro <rshapiro@bbn.com>
2015-06-27 19:17:32 -0400

    First version of replacing tags

---
+++ scripts/replace-tags.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+    echo "Two arguments are required."
+    echo
+    echo "The first argument should be a directory containing tags files."
+    echo "The second argument should be a parallel directory containing flac files"
+    exit -1
+fi
+
+tagsdir="$1"
+flacdir="$2"
+
+if [  ! -d "$flacdir" ]; then
+  echo "The directory "$flacdir" does not exist"
+  exit -2
+fi
+
+if [  ! -d "$tagsdir" ]; then
+  echo "The directory "$tagsdir" does not exist"
+  exit -2
+fi
+
+for file in "$flacdir"/*.flac
+do
+   base=`basename "$file" .flac`
+   tagsfile=${tagsdir}/"${base}".tag
+   tagsbackup=${tagsdir}/"${base}".bak
+   echo "backing up old tags to "$tagsbackup"
+   metaflac --export-tags-to="$tagsbackup" "$file"
+   metaflac --remove-all-tags "$file"
+   metaflac --import-tags-from="$tagsfile" "$file"
+done
