d800436129847518dbba03667a7efc782eee7e4e
R Shapiro <rshapiro@bbn.com>
2015-06-28 09:57:22 -0400

    Fix some comments

--- scripts/down-sample.sh
+++ scripts/down-sample.sh
@@ -4,6 +4,8 @@ if [ $# -lt 2 ]; then
     echo
     echo "This operation down-samples each audio file in a given directory, saving the results to a parallel directory"
     echo "Use this if you have hi-def audio files you want to hear through a player or DAC that only supports CD quality audio"
+    echo 
+    echo "This operation requires sox to be on your PATH"
     echo
     echo "Required arguments:"
     echo
@@ -14,7 +16,7 @@ if [ $# -lt 2 ]; then
     echo
     echo "The desired sample size. The default is 16"
     echo "The desired sample rate. The default is 44100"
-    echo "If the original file has a rate or 96000 and 192000 or you might get better results with 48k instead of 44100"
+    echo "If the original file has a rate or 96000 and 192000 you might get better results with 48k instead of 44100"
     exit 0
 fi
 dir="$1"
