1eea01f72d9a902a67f82a95b97ba41503779fde
R Shapiro <rshapiro@bbn.com>
2015-06-28 12:05:42 -0400

    Note that order matters for now with the optional args.

--- scripts/down-sample.sh
+++ scripts/down-sample.sh
@@ -12,11 +12,12 @@ if [ $# -lt 2 ]; then
     echo "The directory of audio files to be down-sampled."
     echo "The directory in which the down-sampled files will be saved. It will be created if it doesn't exit yet"
     echo
-    echo "Optional arguments:"
+    echo "Optional arguments: note for now order is significant"
+    echo
+    echo "The desired sample rate. The default is 44100"
+    echo "If the original file has a rate of 96000 and 192000 you might get better results with 48k instead of 44100"
     echo
     echo "The desired sample size. The default is 16"
-    echo "The desired sample rate. The default is 44100"
-    echo "If the original file has a rate or 96000 and 192000 you might get better results with 48k instead of 44100"
     exit 0
 fi
 dir="$1"
