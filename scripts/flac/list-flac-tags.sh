0b8d03f9ff797d6a182d0dde6e4d188083ac6066
R Shapiro <rshapiro@bbn.com>
2015-07-11 08:39:53 -0400

    Handle files as well as directories

--- scripts/flac/list-flac-tags.sh
+++ scripts/flac/list-flac-tags.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 1 ]; then
     echo
-    echo "This operation will display all tags of all flac files in a given directory"
+    echo "This operation will display all tags of all flac files in a given file or directory"
     echo "To run this the metaflac command must be on your PATH."
     echo
     echo "Required arguments:"
@@ -13,7 +13,12 @@ fi
 
 dir="$1"
 
-# echo "directory is  "$dir""
+if  [ -f "$dir" ]; then
+# arg is a file, not a directory
+metaflac --export-tags-to=- "$dir"
+exit 0	
+fi
+
 if [  ! -d "$dir" ]; then
   echo "The directory "$dir" does not exist"
   exit -2
@@ -21,8 +26,8 @@ fi
 
 for file in "$dir"/*.flac
 do
-   base=`basename "$file"`
-   echo "$base:"
+   base=`basename "$file" .flac`
+ echo "File: "$base""
    echo
    metaflac --export-tags-to=- "$file"
    echo
